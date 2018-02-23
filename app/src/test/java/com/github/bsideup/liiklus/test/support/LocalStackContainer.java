6039a7cb80083ad2ae1b4d23a1d284e212379872
Sergei Egorov <sergei@uvita.eu>
2018-02-23 15:51:58 +0100

    fix tests on CircleCI

--- app/src/test/java/com/github/bsideup/liiklus/test/support/LocalStackContainer.java
+++ app/src/test/java/com/github/bsideup/liiklus/test/support/LocalStackContainer.java
@@ -1,5 +1,6 @@
 package com.github.bsideup.liiklus.test.support;
 
+import com.amazonaws.SDKGlobalConfiguration;
 import org.testcontainers.containers.GenericContainer;
 
 import java.util.HashMap;
@@ -17,9 +18,9 @@ public class LocalStackContainer extends GenericContainer<LocalStackContainer> {
 
     public Map<String, String> getProperties() {
         Map<String, String> result = new HashMap<>();
-        result.put("aws.region", "eu-central-1");
-        result.put("aws.accessKeyId", "accesskey");
-        result.put("aws.secretAccessKey", "secretkey");
+        result.put(SDKGlobalConfiguration.AWS_REGION_SYSTEM_PROPERTY, "eu-central-1");
+        result.put(SDKGlobalConfiguration.ACCESS_KEY_SYSTEM_PROPERTY, "accesskey");
+        result.put(SDKGlobalConfiguration.SECRET_KEY_SYSTEM_PROPERTY, "secretkey");
         result.put("dynamodb.endpoint", "http://localhost:" + getMappedPort(4569));
         return result;
     }
