23a4e0a9e3abb283733890dcd07807400493b62b
Sergei Egorov <bsideup@gmail.com>
2018-05-11 18:22:41 +0200

    simplify LiiklusPluginRepository.java

--- app/src/main/java/com/github/bsideup/liiklus/plugins/LiiklusPluginRepository.java
+++ app/src/main/java/com/github/bsideup/liiklus/plugins/LiiklusPluginRepository.java
@@ -2,14 +2,14 @@ package com.github.bsideup.liiklus.plugins;
 
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.val;
 import org.pf4j.PluginRepository;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import java.io.IOException;
-import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.nio.file.Path;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @RequiredArgsConstructor
@@ -20,46 +20,19 @@ public class LiiklusPluginRepository implements PluginRepository {
     @Override
     @SneakyThrows
     public List<Path> getPluginPaths() {
-        PathMatcher[] matchers = Stream.of(liiklusPluginManager.getPluginsPathMatcher().split("/"))
-                .map(it -> FileSystems.getDefault().getPathMatcher("glob:" + it))
-                .toArray(PathMatcher[]::new);
+        val pluginsRoot = liiklusPluginManager.getPluginsRoot();
+        val pathMatcher = liiklusPluginManager.getPluginsPathMatcher();
 
-        List<Path> result = new ArrayList<>();
-
-        Files.walkFileTree(liiklusPluginManager.getPluginsRoot(), Collections.emptySet(), matchers.length, new FileVisitor<Path>() {
-
-            int depth = -1;
-
-            @Override
-            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
-                if (depth == -1 || matchers[depth].matches(dir.getFileName())) {
-                    depth++;
-                    return FileVisitResult.CONTINUE;
-                } else {
-                    return FileVisitResult.SKIP_SUBTREE;
-                }
-            }
-
-            @Override
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-                if (matchers[depth].matches(file.getFileName())) {
-                    result.add(file);
-                }
-                return FileVisitResult.CONTINUE;
-            }
-
-            @Override
-            public FileVisitResult visitFileFailed(Path file, IOException exc) {
-                return FileVisitResult.CONTINUE;
-            }
-
-            @Override
-            public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
-                depth--;
-                return FileVisitResult.CONTINUE;
-            }
-        });
-        return result;
+        val locationPattern = "file:" + pluginsRoot.resolve(pathMatcher).toString();
+        return Stream.of(new PathMatchingResourcePatternResolver().getResources(locationPattern))
+                .map(it -> {
+                    try {
+                        return it.getFile().toPath();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .collect(Collectors.toList());
     }
 
     @Override
