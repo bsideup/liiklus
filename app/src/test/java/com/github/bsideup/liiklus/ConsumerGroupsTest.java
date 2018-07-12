83c6ae72c0e02adebb4d6389956c5af618caca28
Sergei Egorov <bsideup@gmail.com>
2018-07-12 11:28:44 +0200

    speed up the tests

--- app/src/test/java/com/github/bsideup/liiklus/ConsumerGroupsTest.java
+++ app/src/test/java/com/github/bsideup/liiklus/ConsumerGroupsTest.java
@@ -1,17 +1,19 @@
 package com.github.bsideup.liiklus;
 
 import com.github.bsideup.liiklus.protocol.PublishRequest;
-import com.github.bsideup.liiklus.protocol.SubscribeReply;
 import com.github.bsideup.liiklus.protocol.SubscribeRequest;
 import com.github.bsideup.liiklus.test.AbstractIntegrationTest;
 import com.google.protobuf.ByteString;
+import lombok.val;
 import org.junit.Before;
 import org.junit.Test;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
 import java.util.Collection;
-import java.util.Map;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -43,15 +45,22 @@ public class ConsumerGroupsTest extends AbstractIntegrationTest {
 
     @Test
     public void testConsumerGroups() {
-        Map<String, Collection<SubscribeReply>> assignments = Flux
+        val assignments = Flux
                 .merge(
                         stub.subscribe(subscribeRequest),
                         stub.subscribe(subscribeRequest)
                 )
                 .distinct(it -> it.getAssignment().getPartition())
-                .take(NUM_PARTITIONS)
-                .collectMultimap(it -> it.getAssignment().getSessionId())
-                .block(Duration.ofSeconds(30));
+                .scanWith(
+                        () -> new HashMap<String, Set<Integer>>(),
+                        (acc, it) -> {
+                            acc.computeIfAbsent(it.getAssignment().getSessionId(), __ -> new HashSet<>()).add(it.getAssignment().getPartition());
+                            return acc;
+                        }
+                )
+                .filter(it -> it.size() == 2)
+                .filter(it -> it.values().stream().flatMap(Collection::stream).distinct().count() == NUM_PARTITIONS)
+                .blockFirst(Duration.ofSeconds(30));
 
         assertThat(assignments)
                 .hasSize(2)
