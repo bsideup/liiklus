4be1cc667faaf4d4858ddb66ca4412fcaa9cdc3d
Sergei Egorov <bsideup@gmail.com>
2018-07-13 14:49:36 +0200

    add `isLatest` to Prometheus exporter

--- app/src/main/java/com/github/bsideup/liiklus/monitoring/MetricsCollector.java
+++ app/src/main/java/com/github/bsideup/liiklus/monitoring/MetricsCollector.java
@@ -2,6 +2,7 @@ package com.github.bsideup.liiklus.monitoring;
 
 import com.github.bsideup.liiklus.config.ExporterProfile;
 import com.github.bsideup.liiklus.positions.PositionsStorage;
+import com.github.bsideup.liiklus.positions.PositionsStorage.Positions;
 import io.prometheus.client.Collector;
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.GaugeMetricFamily;
@@ -15,6 +16,7 @@ import reactor.core.publisher.Flux;
 import javax.annotation.PostConstruct;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -45,16 +47,24 @@ public class MetricsCollector extends Collector {
             return Collections.emptyList();
         }
         return Flux.from(positionsStorage.findAll())
-                .<MetricFamilySamples>map(positions -> {
-                    val gauge = new GaugeMetricFamily("liiklus_topic_position", "", Arrays.asList("topic", "groupId", "groupName", "groupVersion", "partition"));
+                .groupBy(it -> it.getGroupId().getName())
+                .flatMap(it -> it
+                        .sort(Comparator.comparing(Positions::getGroupId).reversed())
+                        .index()
+                )
+                .<MetricFamilySamples>map(tuple -> {
+                    val isLatest = tuple.getT1() == 0;
+                    val positions = tuple.getT2();
+
+                    val gauge = new GaugeMetricFamily("liiklus_topic_position", "", Arrays.asList("topic", "groupName", "groupVersion", "isLatest", "partition"));
 
                     for (val entry : positions.getValues().entrySet()) {
                         gauge.addMetric(
                                 Arrays.asList(
                                         positions.getTopic(),
-                                        positions.getGroupId().asString(),
                                         positions.getGroupId().getName(),
                                         Integer.toString(positions.getGroupId().getVersion().orElse(0)),
+                                        Boolean.toString(isLatest),
                                         entry.getKey().toString()
                                 ),
                                 entry.getValue().doubleValue()
