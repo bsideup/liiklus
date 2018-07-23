9da8a3c73c62b9d8dd0f004cd29bd4f77fb24208
sLawalata <sanga.lawalata@gmail.com>
2018-07-23 17:33:43 +0200

    remove catch and fail in test

--- testing/src/test/java/com/github/bsideup/liiklus/container/LiiklusContainerTest.java
+++ testing/src/test/java/com/github/bsideup/liiklus/container/LiiklusContainerTest.java
@@ -3,8 +3,6 @@ package com.github.bsideup.liiklus.container;
 import org.junit.Test;
 import org.testcontainers.containers.KafkaContainer;
 
-import static org.junit.Assert.fail;
-
 public class LiiklusContainerTest {
 
     static KafkaContainer kafka = new KafkaContainer();
@@ -18,9 +16,6 @@ public class LiiklusContainerTest {
         try (LiiklusContainer liiklusContainer = new LiiklusContainer("0.4.5")) {
 
             liiklusContainer.withKafka(kafka).start();
-
-        } catch (Exception e) {
-            fail(e.getMessage());
         }
     }
 }
