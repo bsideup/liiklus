5fb34f21db24c5c8e9eab1a864b7242702470767
Kirill Merkushev <lanwen@yandex.ru>
2018-08-02 13:59:56 +0200

    naming

--- tck/src/main/java/com/github/bsideup/liiklus/positions/tests/PersistenceTest.java
+++ tck/src/main/java/com/github/bsideup/liiklus/positions/tests/PersistenceTest.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public interface PersistentTest extends PositionsStorageTestSupport {
+public interface PersistenceTest extends PositionsStorageTestSupport {
 
 
     @Test
