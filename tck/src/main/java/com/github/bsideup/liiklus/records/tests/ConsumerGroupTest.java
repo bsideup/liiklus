20b7bbf93f6dc04957e9488bc11102388eb9d73a
Sergei Egorov <bsideup@gmail.com>
2018-10-05 16:16:12 +0200

    More universal TCK

--- tck/src/main/java/com/github/bsideup/liiklus/records/tests/ConsumerGroupTest.java
+++ tck/src/main/java/com/github/bsideup/liiklus/records/tests/ConsumerGroupTest.java
@@ -1,26 +1,47 @@
 package com.github.bsideup.liiklus.records.tests;
 
 import com.github.bsideup.liiklus.records.RecordStorageTestSupport;
-import com.github.bsideup.liiklus.records.RecordsStorage;
+import com.github.bsideup.liiklus.records.RecordsStorage.OffsetInfo;
+import com.github.bsideup.liiklus.records.RecordsStorage.PartitionSource;
+import com.github.bsideup.liiklus.records.RecordsStorage.Subscription;
 import lombok.val;
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
 import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.ReplayProcessor;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public interface ConsumerGroupTest extends RecordStorageTestSupport {
 
     int getNumberOfPartitions();
+
+    String keyByPartition(int partition);
+
+    default Map<Integer, Long> publishToEveryPartition() {
+        return IntStream.range(0, getNumberOfPartitions())
+                .parallel()
+                .mapToObj(partition -> publish(keyByPartition(partition).getBytes(), new byte[1]))
+                .collect(Collectors.toMap(
+                        OffsetInfo::getPartition,
+                        OffsetInfo::getOffset
+                ));
+    }
 
     @Test
     default void testMultipleGroups() throws Exception {
@@ -29,44 +50,122 @@ public interface ConsumerGroupTest extends RecordStorageTestSupport {
 
         val groupName = UUID.randomUUID().toString();
 
-        val assignments = new HashMap<Integer, RecordsStorage.Subscription>();
+        val receivedOffsets = new HashMap<Subscription, Map<Integer, Long>>();
 
         val disposeAll = ReplayProcessor.<Boolean>create(1);
 
-        Function<RecordsStorage.Subscription, Disposable> subscribeAndAssign = subscription -> {
+        Function<Subscription, Disposable> subscribeAndAssign = subscription -> {
             return Flux.from(subscription.getPublisher(() -> CompletableFuture.completedFuture(Collections.emptyMap())))
-                    .flatMap(Flux::fromStream)
+                    .flatMap(Flux::fromStream, numberOfPartitions)
+                    .flatMap(PartitionSource::getPublisher, numberOfPartitions)
                     .takeUntilOther(disposeAll)
-                    .subscribe(source -> assignments.put(source.getPartition(), subscription));
+                    .subscribe(record -> {
+                        receivedOffsets
+                                .computeIfAbsent(subscription, __ -> new HashMap<>())
+                                .put(record.getPartition(), record.getOffset());
+                    });
+        };
+
+        try {
+            val firstSubscription = getTarget().subscribe(getTopic(), groupName, Optional.of("latest"));
+            val secondSubscription = getTarget().subscribe(getTopic(), groupName, Optional.of("latest"));
+            val lastOffsets = new HashMap<Integer, Long>();
+
+            val firstDisposable = subscribeAndAssign.apply(firstSubscription);
+            await.untilAsserted(() -> {
+                try {
+                    assertThat(receivedOffsets).hasEntrySatisfying(firstSubscription, it -> assertThat(it).isEqualTo(lastOffsets));
+                    assertThat(receivedOffsets).doesNotContainKey(secondSubscription);
+                } catch (Throwable e) {
+                    lastOffsets.putAll(publishToEveryPartition());
+                    throw e;
+                }
+            });
+            receivedOffsets.clear();
+
+            val secondDisposable = subscribeAndAssign.apply(secondSubscription);
+            await.untilAsserted(() -> {
+                try {
+                    assertThat(receivedOffsets).hasEntrySatisfying(firstSubscription, it -> assertThat(it).isNotEmpty());
+                    assertThat(receivedOffsets).hasEntrySatisfying(secondSubscription, it -> assertThat(it).isNotEmpty());
+                } catch (Throwable e) {
+                    lastOffsets.putAll(publishToEveryPartition());
+                    throw e;
+                }
+            });
+            receivedOffsets.clear();
+
+            secondDisposable.dispose();
+            await.untilAsserted(() -> {
+                try {
+                    assertThat(receivedOffsets).hasEntrySatisfying(firstSubscription, it -> assertThat(it).isEqualTo(lastOffsets));
+                    assertThat(receivedOffsets).doesNotContainKey(secondSubscription);
+                } catch (Throwable e) {
+                    lastOffsets.putAll(publishToEveryPartition());
+                    throw e;
+                }
+            });
+            receivedOffsets.clear();
+
+            subscribeAndAssign.apply(secondSubscription);
+            firstDisposable.dispose();
+            await.untilAsserted(() -> {
+                try {
+                    assertThat(receivedOffsets).doesNotContainKey(firstSubscription);
+                    assertThat(receivedOffsets).hasEntrySatisfying(secondSubscription, it -> assertThat(it).isEqualTo(lastOffsets));
+                } catch (Throwable e) {
+                    lastOffsets.putAll(publishToEveryPartition());
+                    throw e;
+                }
+            });
+        } finally {
+            disposeAll.onNext(true);
+        }
+    }
+
+    @Test
+    default void testExclusiveRecordDistribution() throws Exception {
+        val numberOfPartitions = getNumberOfPartitions();
+        Assumptions.assumeTrue(numberOfPartitions > 1, "target supports more than 1 partition");
+
+        val groupName = UUID.randomUUID().toString();
+
+        val receivedOffsets = new HashMap<Subscription, Set<Tuple2<Integer, Long>>>();
+
+        val disposeAll = ReplayProcessor.<Boolean>create(1);
+
+        Function<Subscription, Disposable> subscribeAndAssign = subscription -> {
+            return Flux.from(subscription.getPublisher(() -> CompletableFuture.completedFuture(Collections.emptyMap())))
+                    .flatMap(Flux::fromStream, numberOfPartitions)
+                    .flatMap(PartitionSource::getPublisher, numberOfPartitions)
+                    .takeUntilOther(disposeAll)
+                    .subscribe(record -> {
+                        receivedOffsets
+                                .computeIfAbsent(subscription, __ -> new HashSet<>())
+                                .add(Tuples.of(record.getPartition(), record.getOffset()));
+                    });
         };
 
         try {
             val firstSubscription = getTarget().subscribe(getTopic(), groupName, Optional.of("earliest"));
             val secondSubscription = getTarget().subscribe(getTopic(), groupName, Optional.of("earliest"));
 
-            val firstDisposable = subscribeAndAssign.apply(firstSubscription);
-            await.untilAsserted(() -> {
-                assertThat(assignments).hasSize(numberOfPartitions);
-                assertThat(assignments).containsValue(firstSubscription);
-                assertThat(assignments).doesNotContainValue(secondSubscription);
-            });
-
-            val secondDisposable = subscribeAndAssign.apply(secondSubscription);
-            await.untilAsserted(() -> {
-                assertThat(assignments).containsValues(firstSubscription, secondSubscription);
-            });
-
-            secondDisposable.dispose();
-            await.untilAsserted(() -> {
-                assertThat(assignments).doesNotContainValue(secondSubscription);
-            });
-
+            subscribeAndAssign.apply(firstSubscription);
             subscribeAndAssign.apply(secondSubscription);
-            firstDisposable.dispose();
+
             await.untilAsserted(() -> {
-                assertThat(assignments).containsValue(secondSubscription);
-                assertThat(assignments).doesNotContainValue(firstSubscription);
+                try {
+                    assertThat(receivedOffsets)
+                            .containsKeys(firstSubscription, secondSubscription)
+                            .allSatisfy((key, value) -> assertThat(value).isNotEmpty());
+                } catch (Throwable e) {
+                    publishToEveryPartition();
+                    throw e;
+                }
             });
+
+            assertThat(receivedOffsets.get(firstSubscription))
+                    .doesNotContainAnyElementsOf(receivedOffsets.get(secondSubscription));
         } finally {
             disposeAll.onNext(true);
         }
