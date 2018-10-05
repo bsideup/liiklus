e2ea7866c7ae642c2ee79d778a6b971292d8efe1
Sergei Egorov <bsideup@gmail.com>
2018-10-05 17:08:54 +0200

    In-memory records storage

--- app/src/test/java/com/github/bsideup/liiklus/test/AbstractIntegrationTest.java
+++ app/src/test/java/com/github/bsideup/liiklus/test/AbstractIntegrationTest.java
@@ -3,21 +3,16 @@ package com.github.bsideup.liiklus.test;
 import com.github.bsideup.liiklus.Application;
 import com.github.bsideup.liiklus.protocol.ReactorLiiklusServiceGrpc;
 import com.github.bsideup.liiklus.protocol.ReactorLiiklusServiceGrpc.ReactorLiiklusServiceStub;
-import com.github.bsideup.liiklus.test.support.LocalStackContainer;
-import com.google.common.collect.Sets;
 import io.grpc.inprocess.InProcessChannelBuilder;
 import lombok.val;
-import org.apache.kafka.common.utils.Utils;
 import org.junit.Rule;
 import org.junit.rules.TestName;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.KafkaContainer;
 import reactor.core.publisher.Hooks;
 import reactor.core.publisher.Mono;
 
+import java.nio.ByteBuffer;
 import java.time.Duration;
 import java.util.*;
-import java.util.stream.Stream;
 
 public abstract class AbstractIntegrationTest {
 
@@ -39,49 +34,28 @@ public abstract class AbstractIntegrationTest {
     public static Set<String> PARTITION_UNIQUE_KEYS = new HashSet<>(PARTITION_KEYS.values());
 
     public static int getPartitionByKey(String key) {
-        return Utils.toPositive(Utils.murmur2(key.getBytes())) % NUM_PARTITIONS;
+        return Math.abs(ByteBuffer.wrap(key.getBytes()).hashCode()) % NUM_PARTITIONS;
     }
-
-    private static LocalStackContainer localstack = new LocalStackContainer();
-
-    protected static KafkaContainer kafka = new KafkaContainer()
-            .withEnv("KAFKA_NUM_PARTITIONS", NUM_PARTITIONS + "");
 
     protected static ReactorLiiklusServiceStub stub = ReactorLiiklusServiceGrpc.newReactorStub(
             InProcessChannelBuilder.forName("liiklus").build()
     );
 
     static {
-        Stream.of(kafka, localstack).parallel().forEach(GenericContainer::start);
-
-        System.getProperties().putAll(localstack.getProperties());
-
         System.setProperty("server.port", "0");
         System.setProperty("grpc.enabled", "false");
         System.setProperty("plugins.dir", "../plugins");
         System.setProperty("plugins.pathMatcher", "*/build/libs/*.jar");
 
-        val args = new ArrayList<String>();
-        args.add("grpc.inProcessServerName=liiklus");
-        args.add("dynamodb.autoCreateTable=true");
-        args.addAll(getKafkaProperties());
-        args.addAll(getDynamoDBProperties());
+        val args = Arrays.asList(
+                "grpc.inProcessServerName=liiklus",
+                "storage.positions.type=MEMORY",
+                "storage.records.type=MEMORY"
+        );
 
         Application.start(args.stream().map(it -> "--" + it).toArray(String[]::new));
 
         Hooks.onOperatorDebug();
-    }
-
-    public static Set<String> getKafkaProperties() {
-        return Sets.newHashSet(
-                "kafka.bootstrapServers=" + kafka.getBootstrapServers()
-        );
-    }
-
-    public static Set<String> getDynamoDBProperties() {
-        return Sets.newHashSet(
-                "dynamodb.positionsTable=positions"
-        );
     }
 
     @Rule
---
+++ plugins/inmemory-records-storage/src/main/java/com/github/bsideup/liiklus/records/inmemory/InMemoryRecordsStorage.java
@@ -0,0 +1,207 @@
+package com.github.bsideup.liiklus.records.inmemory;
+
+import com.github.bsideup.liiklus.records.RecordsStorage;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import lombok.experimental.FieldDefaults;
+import lombok.val;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxProcessor;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.ReplayProcessor;
+
+import java.nio.ByteBuffer;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * WARNING: this storage type should only be used for testing and NOT in production
+ */
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class InMemoryRecordsStorage implements RecordsStorage {
+
+    public static int partitionByKey(String key, int numberOfPartitions) {
+        return partitionByKey(ByteBuffer.wrap(key.getBytes()), numberOfPartitions);
+    }
+
+    public static int partitionByKey(ByteBuffer key, int numberOfPartitions) {
+        return Math.abs(key.hashCode()) % numberOfPartitions;
+    }
+
+    int numberOfPartitions;
+
+    ConcurrentMap<String, StoredTopic> state = new ConcurrentHashMap<>();
+
+    @Override
+    public CompletionStage<OffsetInfo> publish(Envelope envelope) {
+        val topic = envelope.getTopic();
+        val storedTopic = state.computeIfAbsent(topic, __ -> new StoredTopic(numberOfPartitions));
+
+        val partition = partitionByKey(envelope.getKey(), numberOfPartitions);
+        val storedPartition = storedTopic.getPartitions().computeIfAbsent(
+                partition,
+                __ -> new StoredTopic.StoredPartition()
+        );
+
+        val offset = storedPartition.getNextOffset().getAndIncrement();
+        storedPartition.getProcessor().onNext(new StoredTopic.StoredPartition.StoredRecord(
+                offset,
+                envelope.getKey(),
+                envelope.getValue()
+        ));
+
+        return CompletableFuture.completedFuture(new OffsetInfo(
+                topic,
+                partition,
+                offset
+        ));
+    }
+
+    @Override
+    public Subscription subscribe(String topic, String groupName, Optional<String> autoOffsetReset) {
+        val storedTopic = state.computeIfAbsent(topic, __ -> new StoredTopic(numberOfPartitions));
+        return new Subscription() {
+
+            @Override
+            public Publisher<Stream<? extends PartitionSource>> getPublisher(
+                    Supplier<CompletionStage<Map<Integer, Long>>> offsetsProvider
+            ) {
+                val subscription = this;
+                return Flux.create(sink -> {
+
+                    sink.onCancel(() -> storedTopic.revoke(groupName, this));
+
+                    if (sink.isCancelled()) {
+                        return;
+                    }
+
+                    storedTopic.assign(groupName, subscription);
+
+                    sink.next(IntStream.range(0, numberOfPartitions).mapToObj(partition -> new PartitionSource() {
+
+                        @Override
+                        public int getPartition() {
+                            return partition;
+                        }
+
+                        @Override
+                        public Publisher<Record> getPublisher() {
+                            val storedPartition = storedTopic.getPartitions().computeIfAbsent(
+                                    partition,
+                                    __ -> new StoredTopic.StoredPartition()
+                            );
+                            return Mono.defer(() -> Mono.fromCompletionStage(offsetsProvider.get()))
+                                    .defaultIfEmpty(Collections.emptyMap())
+                                    .flatMapMany(offsets -> storedPartition.getProcessor()
+                                            .compose(flux -> {
+                                                if (offsets.containsKey(partition)) {
+                                                    return flux.skip(offsets.get(partition));
+                                                }
+                                                switch (autoOffsetReset.orElse("")) {
+                                                    case "latest":
+                                                        long nextOffset = storedPartition.getNextOffset().get();
+                                                        if (nextOffset > 0) {
+                                                            return flux.skip(nextOffset);
+                                                        }
+                                                    default:
+                                                        return flux;
+                                                }
+                                            })
+                                    )
+                                    .filter(it -> storedTopic.isAssigned(groupName, subscription, partition))
+                                    .map(it -> new Record(
+                                            new Envelope(
+                                                    topic,
+                                                    it.getKey().asReadOnlyBuffer(),
+                                                    it.getValue().asReadOnlyBuffer()
+                                            ),
+                                            it.getTimestamp(),
+                                            partition,
+                                            it.getOffset()
+                                    ));
+                        }
+                    }));
+                });
+            }
+        };
+    }
+
+    @Value
+    static class StoredTopic {
+
+        int numberOfPartitions;
+
+        ConcurrentMap<Integer, StoredPartition> partitions = new ConcurrentHashMap<>();
+
+        ConcurrentMap<String, ConcurrentMap<Subscription, Set<Integer>>> groupAssignments = new ConcurrentHashMap<>();
+
+        synchronized void assign(String groupName, Subscription subscription) {
+            val groupAssignment = groupAssignments.computeIfAbsent(groupName, ___ -> new ConcurrentHashMap<>());
+            groupAssignment.put(subscription, Collections.emptySet());
+            rebalance(groupName);
+        }
+
+        synchronized void revoke(String groupName, Subscription subscription) {
+            val groupAssignment = groupAssignments.computeIfAbsent(groupName, ___ -> new ConcurrentHashMap<>());
+            groupAssignment.remove(subscription);
+            rebalance(groupName);
+        }
+
+        synchronized boolean isAssigned(String groupName, Subscription subscription, int partition) {
+            return groupAssignments.get(groupName).get(subscription).contains(partition);
+        }
+
+        private synchronized void rebalance(String groupName) {
+            val subscriptions = groupAssignments.get(groupName);
+
+            val i = new AtomicLong();
+            int subscriptionsNum = subscriptions.size();
+            for (val subscription : subscriptions.keySet()) {
+                val entryNum = i.getAndIncrement();
+                subscriptions.put(
+                        subscription,
+                        IntStream
+                                .range(0, numberOfPartitions)
+                                .filter(it -> it % subscriptionsNum == entryNum)
+                                .boxed()
+                                .collect(Collectors.toSet())
+                );
+            }
+        }
+
+        @Value
+        static class StoredPartition {
+
+            AtomicLong nextOffset = new AtomicLong(0);
+
+            FluxProcessor<StoredRecord, StoredRecord> processor = ReplayProcessor.create(Integer.MAX_VALUE);
+
+            @Value
+            static class StoredRecord {
+
+                Instant timestamp = Instant.now();
+
+                long offset;
+
+                ByteBuffer key;
+
+                ByteBuffer value;
+            }
+        }
+    }
+}
