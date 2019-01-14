eb1d98949f72ee12a6853c0b10607e5a218bd614
Kirill Merkushev <lanwen@yandex.ru>
2019-01-14 12:09:02 +0100

    DynamoDB with aws sdk v2 (#77)
    * DynamoDB with aws sdk v2
    
    * unwrap CompletionException
    
    * get rid of one defer
    
    * add region to tests
    
    * exclude slf4j from test ddb
    
    * exclude slf4j from compile ddb
    
    * get rid of defer
    
    * dont exclude slf4j from test
    
    * awssdk2 2.3.0, exclude `org.reactivestreams`

--- plugins/dynamodb-positions-storage/src/main/java/com/github/bsideup/liiklus/dynamodb/DynamoDBPositionsStorage.java
+++ plugins/dynamodb-positions-storage/src/main/java/com/github/bsideup/liiklus/dynamodb/DynamoDBPositionsStorage.java
@@ -1,47 +1,41 @@
 package com.github.bsideup.liiklus.dynamodb;
 
-import com.amazonaws.AmazonWebServiceRequest;
-import com.amazonaws.handlers.AsyncHandler;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsync;
-import com.amazonaws.services.dynamodbv2.model.AttributeAction;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.AttributeValueUpdate;
-import com.amazonaws.services.dynamodbv2.model.Condition;
-import com.amazonaws.services.dynamodbv2.model.ConditionalCheckFailedException;
-import com.amazonaws.services.dynamodbv2.model.ExpectedAttributeValue;
-import com.amazonaws.services.dynamodbv2.model.GetItemRequest;
-import com.amazonaws.services.dynamodbv2.model.GetItemResult;
-import com.amazonaws.services.dynamodbv2.model.QueryRequest;
-import com.amazonaws.services.dynamodbv2.model.QueryResult;
-import com.amazonaws.services.dynamodbv2.model.ScanRequest;
-import com.amazonaws.services.dynamodbv2.model.ScanResult;
-import com.amazonaws.services.dynamodbv2.model.UpdateItemRequest;
-import com.amazonaws.services.dynamodbv2.model.UpdateItemResult;
-import com.amazonaws.util.ImmutableMapParameter;
 import com.github.bsideup.liiklus.positions.GroupId;
 import com.github.bsideup.liiklus.positions.PositionsStorage;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.core.publisher.MonoSink;
 import reactor.core.publisher.SignalType;
+import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeAction;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValueUpdate;
+import software.amazon.awssdk.services.dynamodb.model.ComparisonOperator;
+import software.amazon.awssdk.services.dynamodb.model.Condition;
+import software.amazon.awssdk.services.dynamodb.model.ConditionalCheckFailedException;
+import software.amazon.awssdk.services.dynamodb.model.ExpectedAttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.GetItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.QueryRequest;
+import software.amazon.awssdk.services.dynamodb.model.QueryResponse;
+import software.amazon.awssdk.services.dynamodb.model.ScanResponse;
+import software.amazon.awssdk.services.dynamodb.model.UpdateItemRequest;
+import software.amazon.awssdk.utils.ImmutableMap;
 
 import java.time.Duration;
 import java.util.AbstractMap;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.CompletionStage;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Level;
 import java.util.stream.Collectors;
 
-import static com.amazonaws.services.dynamodbv2.model.ComparisonOperator.BEGINS_WITH;
-import static com.amazonaws.services.dynamodbv2.model.ComparisonOperator.EQ;
 import static java.util.Collections.emptyMap;
 import static java.util.Collections.singletonMap;
+import static software.amazon.awssdk.services.dynamodb.model.ComparisonOperator.BEGINS_WITH;
+import static software.amazon.awssdk.services.dynamodb.model.ComparisonOperator.EQ;
 
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true)
@@ -51,57 +45,33 @@ public class DynamoDBPositionsStorage implements PositionsStorage {
 
     public static final String RANGE_KEY_FIELD = "groupId";
 
-    AmazonDynamoDBAsync dynamoDB;
+    DynamoDbAsyncClient dynamoDB;
 
     String tableName;
 
     @Override
     public Publisher<Positions> findAll() {
-        var request = new ScanRequest(tableName);
-
-        AtomicBoolean done = new AtomicBoolean(false);
-
-        return Mono
-                .<ScanResult>create(sink -> dynamoDB.scanAsync(request, new DefaultAsyncHandler<>(sink)))
-                .<List<Map<String, AttributeValue>>>handle(((scanResult, sink) -> {
-                    try {
-                        if (scanResult.getCount() <= 0) {
-                            done.set(true);
-                            sink.complete();
-                        } else {
-                            if (scanResult.getLastEvaluatedKey() == null || scanResult.getLastEvaluatedKey().isEmpty()) {
-                                done.set(true);
-                            }
-                            sink.next(scanResult.getItems());
-                        }
-                    } catch (Exception e) {
-                        sink.error(e);
-                    }
-                }))
-                .repeat(() -> !done.get())
-                .flatMapIterable(it -> it)
+        return Flux.from(dynamoDB.scanPaginator(req -> req.tableName(tableName)))
+                .flatMapIterable(ScanResponse::items)
                 .map(item -> new Positions(
-                        item.get("topic").getS(),
-                        GroupId.ofString(item.get("groupId").getS()),
+                        item.get("topic").s(),
+                        GroupId.ofString(item.get("groupId").s()),
                         toPositions(item)
                 ));
     }
 
     @Override
     public CompletionStage<Map<Integer, Long>> findAll(String topic, GroupId groupId) {
-        var request = new GetItemRequest()
-                .withTableName(tableName)
-                .withConsistentRead(true)
-                .withKey(toKey(topic, groupId));
+        var request = GetItemRequest.builder()
+                .tableName(tableName)
+                .consistentRead(true)
+                .key(toKey(topic, groupId))
+                .build();
 
-        return Mono
-                .<GetItemResult>create(sink -> {
-                    var future = dynamoDB.getItemAsync(request, new DefaultAsyncHandler<>(sink));
-                    sink.onCancel(() -> future.cancel(true));
-                })
+        return Mono.fromCompletionStage(() -> dynamoDB.getItem(request))
                 .<Map<Integer, Long>>handle((result, sink) -> {
                     try {
-                        var positions = toPositions(result.getItem());
+                        var positions = toPositions(result.item());
 
                         if (positions == null) {
                             sink.complete();
@@ -119,28 +89,19 @@ public class DynamoDBPositionsStorage implements PositionsStorage {
 
     @Override
     public CompletionStage<Map<Integer, Map<Integer, Long>>> findAllVersionsByGroup(String topic, String groupName) {
-        var request = new QueryRequest(tableName)
-                .addKeyConditionsEntry(HASH_KEY_FIELD, new Condition().withComparisonOperator(EQ).withAttributeValueList(new AttributeValue(topic)))
-                .addKeyConditionsEntry(RANGE_KEY_FIELD, new Condition().withComparisonOperator(BEGINS_WITH).withAttributeValueList(new AttributeValue(groupName)));
+        var request = QueryRequest.builder()
+                .tableName(tableName)
+                .keyConditions(ImmutableMap.of(
+                        HASH_KEY_FIELD, condition(EQ, attribute(topic)),
+                        RANGE_KEY_FIELD, condition(BEGINS_WITH, attribute(groupName))
+                ))
+                .build();
 
-        AtomicBoolean done = new AtomicBoolean(false);
-
-        return Mono
-                .<QueryResult>create(sink -> dynamoDB.queryAsync(request, new DefaultAsyncHandler<>(sink)))
-                .<List<Map<String, AttributeValue>>>handle((result, sink) -> {
-                    try {
-                        if (result.getCount() <= 0 || result.getLastEvaluatedKey() == null || result.getLastEvaluatedKey().isEmpty()) {
-                            done.set(true);
-                        }
-                        sink.next(result.getItems());
-                    } catch (Exception e) {
-                        sink.error(e);
-                    }
-                })
-                .repeat(() -> !done.get())
-                .flatMapIterable(it -> it)
+        return Flux
+                .from(dynamoDB.queryPaginator(request))
+                .flatMapIterable(QueryResponse::items)
                 .map(item -> new AbstractMap.SimpleEntry<>(
-                        GroupId.ofString(item.get("groupId").getS()),
+                        GroupId.ofString(item.get("groupId").s()),
                         toPositions(item)
                 ))
                 .filter(it -> groupName.equals(it.getKey().getName()))
@@ -155,49 +116,44 @@ public class DynamoDBPositionsStorage implements PositionsStorage {
     public CompletionStage<Void> update(String topic, GroupId groupId, int partition, long position) {
         Map<String, AttributeValue> key = toKey(topic, groupId);
 
-        return Mono
-                .<UpdateItemResult>create(sink -> {
-                    var request = new UpdateItemRequest()
-                            .withTableName(tableName)
-                            .withKey(key)
-                            .withConditionExpression("attribute_exists(positions)")
-                            .withUpdateExpression("SET positions.#p = :pos")
-                            .withExpressionAttributeNames(ImmutableMapParameter.of(
-                                    "#p", Integer.toString(partition)
-                            ))
-                            .withExpressionAttributeValues(ImmutableMapParameter.of(
-                                    ":pos", new AttributeValue().withN(Long.toString(position))
-                            ));
+        var update = UpdateItemRequest.builder()
+                .tableName(tableName)
+                .key(key)
+                .conditionExpression("attribute_exists(positions)")
+                .updateExpression("SET positions.#p = :pos")
+                .expressionAttributeNames(ImmutableMap.of(
+                        "#p", Integer.toString(partition)
+                ))
+                .expressionAttributeValues(ImmutableMap.of(
+                        ":pos", AttributeValue.builder().n(Long.toString(position)).build()
+                ))
+                .build();
 
-                    var future = dynamoDB.updateItemAsync(request, new DefaultAsyncHandler<>(sink));
-
-                    sink.onCancel(() -> future.cancel(true));
-                })
+        return Mono.fromCompletionStage(() -> dynamoDB.updateItem(update))
                 .then()
+                .onErrorMap(CompletionException.class, CompletionException::getCause)
                 .retryWhen(it -> it.delayUntil(e -> {
                     if (!(e instanceof ConditionalCheckFailedException)) {
                         return Mono.error(e);
                     }
 
-                    return Mono
-                            .<UpdateItemResult>create(sink -> {
-                                // TODO find out how to do field UPSERT with DynamoDB's expressions
-                                var request = new UpdateItemRequest()
-                                        .withTableName(tableName)
-                                        .withKey(key)
-                                        .withAttributeUpdates(singletonMap(
-                                                "positions",
-                                                new AttributeValueUpdate()
-                                                        .withAction(AttributeAction.PUT)
-                                                        .withValue(new AttributeValue().withM(emptyMap()))
-                                        ))
-                                        .withExpected(singletonMap("positions", new ExpectedAttributeValue(false)));
+                    // TODO find out how to do field UPSERT with DynamoDB's expressions
+                    var create = UpdateItemRequest.builder()
+                            .tableName(tableName)
+                            .key(key)
+                            .attributeUpdates(singletonMap(
+                                    "positions",
+                                    AttributeValueUpdate.builder()
+                                            .action(AttributeAction.PUT)
+                                            .value(AttributeValue.builder().m(emptyMap()).build())
+                                            .build()
+                            ))
+                            .expected(singletonMap("positions", ExpectedAttributeValue.builder().exists(false).build()))
+                            .build();
 
-                                var future = dynamoDB.updateItemAsync(request, new DefaultAsyncHandler<>(sink));
-
-                                sink.onCancel(() -> future.cancel(true));
-                            })
+                    return Mono.fromCompletionStage(() -> dynamoDB.updateItem(create))
                             .map(__ -> true)
+                            .onErrorMap(CompletionException.class, CompletionException::getCause)
                             .onErrorResume(ConditionalCheckFailedException.class, __ -> Mono.just(true));
                 }))
                 .log(this.getClass().getName(), Level.WARNING, SignalType.ON_ERROR)
@@ -205,44 +161,36 @@ public class DynamoDBPositionsStorage implements PositionsStorage {
                 .toFuture();
     }
 
-    Map<String, AttributeValue> toKey(String topic, GroupId groupId) {
+    static Map<String, AttributeValue> toKey(String topic, GroupId groupId) {
         var result = new HashMap<String, AttributeValue>();
-        result.put(HASH_KEY_FIELD, new AttributeValue(topic));
-        result.put(RANGE_KEY_FIELD, new AttributeValue(groupId.asString()));
+        result.put(HASH_KEY_FIELD, attribute(topic));
+        result.put(RANGE_KEY_FIELD, attribute(groupId.asString()));
         return result;
     }
 
-    Map<Integer, Long> toPositions(Map<String, AttributeValue> item) {
+    static Map<Integer, Long> toPositions(Map<String, AttributeValue> item) {
         if (item == null) {
             return null;
         }
 
         AttributeValue positions = item.get("positions");
 
-        if (positions == null || positions.getM() == null) {
+        if (positions == null || positions.m() == null) {
             return null;
         }
 
-        return positions.getM().entrySet().stream()
+        return positions.m().entrySet().stream()
                 .collect(Collectors.toMap(
                         it -> Integer.parseInt(it.getKey()),
-                        it -> Long.parseLong(it.getValue().getN())
+                        it -> Long.parseLong(it.getValue().n())
                 ));
     }
 
+    static Condition condition(ComparisonOperator eq, AttributeValue attribute) {
+        return Condition.builder().comparisonOperator(eq).attributeValueList(attribute).build();
+    }
 
-    @AllArgsConstructor
-    public static class DefaultAsyncHandler<REQUEST extends AmazonWebServiceRequest, RESULT> implements AsyncHandler<REQUEST, RESULT> {
-        protected MonoSink<RESULT> sink;
-
-        @Override
-        public void onError(Exception exception) {
-            sink.error(exception);
-        }
-
-        @Override
-        public void onSuccess(REQUEST request, RESULT result) {
-            sink.success(result);
-        }
+    static AttributeValue attribute(String topic) {
+        return AttributeValue.builder().s(topic).build();
     }
 }
