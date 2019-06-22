a86fd75c85f043643153c3044686197f5d0c81e1
Sergei Egorov <segorov@pivotal.io>
2019-06-22 15:29:17 +0200

    update the configuration (use programmatic style)

--- plugins/pulsar-records-storage/src/main/java/com/github/bsideup/liiklus/pulsar/config/PulsarRecordsStorageConfiguration.java
+++ plugins/pulsar-records-storage/src/main/java/com/github/bsideup/liiklus/pulsar/config/PulsarRecordsStorageConfiguration.java
@@ -1,43 +1,54 @@
 package com.github.bsideup.liiklus.pulsar.config;
 
-import com.github.bsideup.liiklus.config.GatewayProfile;
-import com.github.bsideup.liiklus.config.LiiklusConfiguration;
 import com.github.bsideup.liiklus.pulsar.PulsarRecordsStorage;
 import com.github.bsideup.liiklus.records.RecordsStorage;
 import com.google.auto.service.AutoService;
 import lombok.Data;
+import lombok.SneakyThrows;
 import org.apache.pulsar.client.api.PulsarClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.apache.pulsar.client.api.PulsarClientException;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.core.env.Profiles;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotEmpty;
+import java.util.function.Supplier;
 
-@AutoService(LiiklusConfiguration.class)
-@Configuration
-@GatewayProfile
-@EnableConfigurationProperties(PulsarRecordsStorageConfiguration.PulsarProperties.class)
-@ConditionalOnProperty(value = "storage.records.type", havingValue = "PULSAR")
-public class PulsarRecordsStorageConfiguration implements LiiklusConfiguration {
+@AutoService(ApplicationContextInitializer.class)
+public class PulsarRecordsStorageConfiguration implements ApplicationContextInitializer<GenericApplicationContext> {
 
-    @Autowired
-    PulsarProperties pulsarProperties;
+    @Override
+    public void initialize(GenericApplicationContext applicationContext) {
+        var environment = applicationContext.getEnvironment();
 
-    @Bean
-    RecordsStorage reactorPulsarSource() throws Exception {
-        return new PulsarRecordsStorage(
-                PulsarClient.builder().serviceUrl(pulsarProperties.getServiceUrl()).build()
-        );
+        if (!environment.acceptsProfiles(Profiles.of("gateway"))) {
+            return;
+        }
+
+        if (!"PULSAR".equals(environment.getProperty("storage.records.type"))) {
+            return;
+        }
+
+        var binder = Binder.get(environment);
+
+        var pulsarProperties = binder.bind("pulsar", PulsarProperties.class).get();
+
+        applicationContext.registerBean(RecordsStorage.class, new Supplier<RecordsStorage>() {
+            @Override
+            @SneakyThrows(PulsarClientException.class)
+            public RecordsStorage get() {
+                return new PulsarRecordsStorage(
+                        PulsarClient.builder().serviceUrl(pulsarProperties.getServiceUrl()).build()
+                );
+            }
+        });
     }
 
     @Data
-    @ConfigurationProperties("pulsar")
     @Validated
-    public static class PulsarProperties {
+    private static class PulsarProperties {
 
         @NotEmpty
         String serviceUrl;
