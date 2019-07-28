8982e01ad7e607972dd1c26ae651efd2730ce0fe
Sergei Egorov <segorov@pivotal.io>
2019-07-28 17:19:37 +0200

    Add ClassLoader isolation to `ApplicationRunner`

--- plugins/inmemory-records-storage/src/test/java/com/github/bsideup/liiklus/records/inmemory/InMemoryRecordsStorageTest.java
+++ plugins/inmemory-records-storage/src/test/java/com/github/bsideup/liiklus/records/inmemory/InMemoryRecordsStorageTest.java
@@ -1,12 +1,10 @@
-package com.github.bsideup.liiklus.kafka;
+package com.github.bsideup.liiklus.records.inmemory;
 
 import com.github.bsideup.liiklus.ApplicationRunner;
 import com.github.bsideup.liiklus.records.RecordStorageTests;
 import com.github.bsideup.liiklus.records.RecordsStorage;
 import lombok.Getter;
-import org.apache.kafka.common.utils.Utils;
 import org.springframework.context.ApplicationContext;
-import org.testcontainers.containers.KafkaContainer;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
@@ -14,9 +12,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-public class KafkaRecordsStorageTest implements RecordStorageTests {
+class InMemoryRecordsStorageTest implements RecordStorageTests {
 
-    private static final int NUM_OF_PARTITIONS = 4;
+    private static final int NUM_OF_PARTITIONS = 32;
 
     // Generate a set of keys where each key goes to unique partition
     public static Map<Integer, String> PARTITION_KEYS = Mono.fromCallable(() -> UUID.randomUUID().toString())
@@ -24,25 +22,14 @@ public class KafkaRecordsStorageTest implements RecordStorageTests {
             .scanWith(
                     () -> new HashMap<Integer, String>(),
                     (acc, it) -> {
-                        acc.put(Utils.toPositive(Utils.murmur2(it.getBytes())) % NUM_OF_PARTITIONS, it);
+                        acc.put(InMemoryRecordsStorage.partitionByKey(it, NUM_OF_PARTITIONS), it);
                         return acc;
                     }
             )
             .filter(it -> it.size() == NUM_OF_PARTITIONS)
             .blockFirst(Duration.ofSeconds(10));
 
-    private static final KafkaContainer kafka = new KafkaContainer()
-            .withEnv("KAFKA_NUM_PARTITIONS", NUM_OF_PARTITIONS + "");
-
-    static final ApplicationContext applicationContext;
-
-    static {
-        kafka.start();
-
-        System.setProperty("kafka.bootstrapServers", kafka.getBootstrapServers());
-
-        applicationContext = new ApplicationRunner("KAFKA", "MEMORY").run();
-    }
+    static final ApplicationContext applicationContext = new ApplicationRunner("MEMORY", "MEMORY").run();
 
     @Getter
     RecordsStorage target = applicationContext.getBean(RecordsStorage.class);
@@ -51,12 +38,12 @@ public class KafkaRecordsStorageTest implements RecordStorageTests {
     String topic = UUID.randomUUID().toString();
 
     @Override
-    public int getNumberOfPartitions() {
-        return NUM_OF_PARTITIONS;
+    public String keyByPartition(int partition) {
+        return PARTITION_KEYS.get(partition);
     }
 
     @Override
-    public String keyByPartition(int partition) {
-        return PARTITION_KEYS.get(partition);
+    public int getNumberOfPartitions() {
+        return NUM_OF_PARTITIONS;
     }
 }
