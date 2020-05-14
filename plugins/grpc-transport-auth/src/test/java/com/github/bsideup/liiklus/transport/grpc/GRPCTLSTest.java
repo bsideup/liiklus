81bf4087e5e8aec109654139894f011b3cda88ba
Kirill Merkushev <lanwen@yandex.ru>
2020-05-14 22:48:28 +0200

    generate certs following
    https://github.com/grpc/grpc-java/tree/master/testing/src/main/resources/certs

--- plugins/grpc-transport-auth/src/test/java/com/github/bsideup/liiklus/transport/grpc/GRPCTLSTest.java
+++ plugins/grpc-transport-auth/src/test/java/com/github/bsideup/liiklus/transport/grpc/GRPCTLSTest.java
@@ -4,7 +4,6 @@ import com.github.bsideup.liiklus.ApplicationRunner;
 import com.github.bsideup.liiklus.GRPCLiiklusClient;
 import com.github.bsideup.liiklus.protocol.LiiklusEvent;
 import com.github.bsideup.liiklus.protocol.PublishRequest;
-import com.google.protobuf.ByteString;
 import io.grpc.StatusRuntimeException;
 import io.grpc.netty.GrpcSslContexts;
 import io.grpc.netty.NettyChannelBuilder;
@@ -13,25 +12,38 @@ import org.springframework.util.ResourceUtils;
 
 import javax.net.ssl.SSLException;
 import java.io.FileNotFoundException;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 
 import static com.github.bsideup.liiklus.transport.grpc.GRPCAuthTest.getGRPCPort;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class GRPCTLSTest {
 
+    private static final LiiklusEvent LIIKLUS_EVENT_EXAMPLE = LiiklusEvent.newBuilder()
+            .setId(UUID.randomUUID().toString())
+            .setType("com.example.event")
+            .setSource("/tests")
+            .setDataContentType("application/json")
+            .putExtensions("comexampleextension1", "foo")
+            .putExtensions("comexampleextension2", "bar")
+            .setTime(ZonedDateTime.now().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME))
+            .buildPartial();
+
     @Test
     public void shouldConnectWithTLS() throws SSLException, FileNotFoundException {
         var event = PublishRequest.newBuilder()
                 .setTopic("authorized")
-                .setLiiklusEvent(LiiklusEvent.newBuilder().setData(ByteString.copyFromUtf8("bar")).build())
+                .setLiiklusEvent(LIIKLUS_EVENT_EXAMPLE)
                 .build();
 
         try (var app = new ApplicationRunner("MEMORY", "MEMORY")
                 .withProperty("grpc.enabled", true)
                 .withProperty("grpc.port", 0)
-                .withProperty("grpc.tls.key", "file:/Users/lanwen/code/github.com/bsideup/liiklus/pki/private/server.pkcs8.key") //didn't get where relative path
+                .withProperty("grpc.tls.key", "file:src/test/resources/keys/tls/server0.key")
 //                .withProperty("grpc.tls.keyPassword", "testsecret")
-                .withProperty("grpc.tls.keyCertChain", "file:/Users/lanwen/code/github.com/bsideup/liiklus/pki/issued/server.crt")
+                .withProperty("grpc.tls.keyCertChain", "file:src/test/resources/keys/tls/server0.pem")
                 .run()
         ) {
             int port = getGRPCPort(app);
@@ -41,7 +53,7 @@ public class GRPCTLSTest {
                             .forAddress("localhost", port)
                             .directExecutor()
                             .sslContext(GrpcSslContexts.forClient()
-                                    .trustManager(ResourceUtils.getFile("file:/Users/lanwen/code/github.com/bsideup/liiklus/pki/issued/server.crt"))
+                                    .trustManager(ResourceUtils.getFile("file:src/test/resources/keys/tls/ca.pem"))
                                     .build()
                             )
                             .build()
